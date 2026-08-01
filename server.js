8c3c12175a74ce4d48660a6bfcb2e81fc1354b53
Saran <smsaran@gmail.com>
2026-08-01 23:23:43 +0530

    Fix express route syntax

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { maxHttpBufferSize: 1e7 });
 
-// Serve static files from 'public' folder
+// Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home Route Fix - sending public/index.html
-app.get('*', (req, res) => {
+// Home Route Fix (Express 4/5 compatible route)
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
