07b800ec409be569c495cd87f2547365bfb8de58
Saran <smsaran@gmail.com>
2026-08-01 22:50:54 +0530

    Fix homepage route

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { maxHttpBufferSize: 1e7 }); // 10MB Image Upload Support
+const io = new Server(server, { maxHttpBufferSize: 1e7 });
 
+// Serve static files
 app.use(express.static(__dirname));
+
+// Home Route Fix (Cannot GET / Error Solution)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let activeUsers = [];
 
