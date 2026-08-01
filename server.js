66fd7119b83b58694ca7e65e149504168db98065
Saran <smsaran@gmail.com>
2026-08-01 23:37:13 +0530

    Add Friend Request and Private Messaging

--- server.js
+++ server.js
@@ -7,33 +7,95 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { maxHttpBufferSize: 1e7 });
 
-// Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home Route Fix (Express 4/5 compatible route)
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-let activeUsers = [];
+// In-Memory Data Storage
+const users = {}; // socketId -> username
+const userSockets = {}; // username -> socketId
+const friendRequests = {}; // username -> array of requests from users
+const userFriends = {}; // username -> array of friend usernames
 
 io.on('connection', (socket) => {
-  socket.on('userJoined', (data) => {
-    socket.userData = data;
-    activeUsers.push(data);
-    io.emit('updateUsers', activeUsers);
+  
+  socket.on('registerUser', (username) => {
+    users[socket.id] = username;
+    userSockets[username] = socket.id;
+
+    if (!friendRequests[username]) friendRequests[username] = [];
+    if (!userFriends[username]) userFriends[username] = [];
+
+    broadcastUserData();
   });
 
-  socket.on('chatMessage', (data) => {
-    io.emit('message', data);
+  // Friend Request Handlers
+  socket.on('sendFriendRequest', ({ from, to }) => {
+    if (friendRequests[to] && !friendRequests[to].includes(from)) {
+      friendRequests[to].push(from);
+      if (userSockets[to]) {
+        sendUserDataToUser(to);
+      }
+    }
+  });
+
+  socket.on('acceptFriendRequest', ({ me, friend }) => {
+    // Add to each other's friend lists
+    if (!userFriends[me].includes(friend)) userFriends[me].push(friend);
+    if (!userFriends[friend]) userFriends[friend] = [];
+    if (!userFriends[friend].includes(me)) userFriends[friend].push(me);
+
+    // Remove from request list
+    friendRequests[me] = friendRequests[me].filter(r => r !== friend);
+
+    sendUserDataToUser(me);
+    if (userSockets[friend]) sendUserDataToUser(friend);
+  });
+
+  socket.on('rejectFriendRequest', ({ me, friend }) => {
+    friendRequests[me] = friendRequests[me].filter(r => r !== friend);
+    sendUserDataToUser(me);
+  });
+
+  // Messaging Handlers
+  socket.on('globalMessage', (data) => {
+    io.emit('receiveGlobalMessage', data);
+  });
+
+  socket.on('privateMessage', ({ sender, to, text }) => {
+    const targetSocketId = userSockets[to];
+    if (targetSocketId) {
+      io.to(targetSocketId).emit('receivePrivateMessage', { sender, text });
+    }
   });
 
   socket.on('disconnect', () => {
-    if (socket.userData) {
-      activeUsers = activeUsers.filter(u => u.name !== socket.userData.name);
-      io.emit('updateUsers', activeUsers);
+    const username = users[socket.id];
+    if (username) {
+      delete userSockets[username];
+      delete users[socket.id];
+      broadcastUserData();
     }
   });
+
+  function sendUserDataToUser(username) {
+    const sId = userSockets[username];
+    if (sId) {
+      io.to(sId).emit('updateUserData', {
+        onlineUsers: Object.values(users),
+        requests: friendRequests[username] || [],
+        friends: userFriends[username] || []
+      });
+    }
+  }
+
+  function broadcastUserData() {
+    Object.keys(userSockets).forEach(u => {
+      sendUserDataToUser(u);
+    });
+  }
 });
 
 const PORT = process.env.PORT || 80;
